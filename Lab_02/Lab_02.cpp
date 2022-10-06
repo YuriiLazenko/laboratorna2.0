424a41a6f2f649651392c8c21a98d8a0892aeb4c
Yurii <steamyurilazenko@gmail.com>
2022-10-06 21:47:41 +0300

    second version

--- Lab_02/Lab_02.cpp
+++ Lab_02/Lab_02.cpp
@@ -11,12 +11,12 @@ int main()
 
     cout << "a = "; cin >> a;
 
-    z1 = ((sin(4 * a)) / 1 + cos(4 * a)) * (cos(2 * a) / 1 + cos(2 * a));
+    //z1 = ((sin(4 * a)) / 1 + cos(4 * a)) * (cos(2 * a) / 1 + cos(2 * a));
     z2 = 1 / tan((3 / 2) * Pi - a);
 
     cin.get();
     cout << endl;
-    cout << "z1 = " << z1 << endl;
+    //cout << "z1 = " << z1 << endl;
     cout << "z2 = " << z2 << endl;
     cin.get();
     return 0;
